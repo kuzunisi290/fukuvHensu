1045f8064f78915c57634664dc712dc6632edace
saito-dat <kuroutoFOV90@gmail.com>
2022-06-16 13:44:13 +0900

    Initial commit

---
+++ fukuvHensu/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukuvHensu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left-= int.Parse(textBox1.Text);
+            label1.Top -= int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = ("10");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = ("-10");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ("10");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ("-10");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("葛西達也");
+        }
+    }
+}
