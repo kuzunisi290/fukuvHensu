24b2b70dc3803f0e73ece956f8ec5c000847afbe
saito-dat <kuroutoFOV90@gmail.com>
2022-06-16 14:13:37 +0900

    syuusei&omake

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -12,6 +12,8 @@ namespace fukuvHensu
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
         public Form1()
         {
             InitializeComponent();
@@ -19,28 +21,32 @@ namespace fukuvHensu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left-= int.Parse(textBox1.Text);
-            label1.Top -= int.Parse(textBox2.Text);
+            label1.Left-= vx;
+            label1.Top -= vy;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox2.Text = ("10");
+            vy = 10;
+            vx = 0;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox2.Text = ("-10");
+            vy = -10;
+            vx = 0;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = ("10");
+            vx = 10;
+            vy =0;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = ("-10");
+            vx = -10;
+            vy = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -51,6 +57,7 @@ namespace fukuvHensu
         private void label1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("葛西達也");
+            timer1.Enabled = false;
         }
     }
 }
